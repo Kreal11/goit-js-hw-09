dbab86a05d423b3c0bdc695d41ab327c2a8a55f2
Kreal11 <140453416+Kreal11@users.noreply.github.com>
2023-09-26 12:01:17 +0000

    Deploying to gh-pages from @ Kreal11/goit-js-hw-09@fe3e76c96051b34333380ed59d03ed907372cf19 🚀

--- 01-color-switcher.8715022e.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");let o=null;t.addEventListener("click",(e=>{o=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(e=>{clearInterval(o),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.8715022e.js.map
--- 01-color-switcher.a67eeb39.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;t.addEventListener("click",(function(e){o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0})),e.addEventListener("click",(function(e){clearInterval(o),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.a67eeb39.js.map
