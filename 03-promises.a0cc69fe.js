bdbd6f7090786921fd5bbc6503c68798a26c208d
Kreal11 <140453416+Kreal11@users.noreply.github.com>
2023-09-27 10:13:11 +0000

    Deploying to gh-pages from @ Kreal11/goit-js-hw-09@0191299f712c14b6c9a3228d63d5c2434c0153e2 🚀

--- 03-promises.a0cc69fe.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('button[type = "submit"]').addEventListener("submit",(function(t){var e,n;t.preventDefault(),new Promise((function(t,o){var i=Math.random()>.3;setTimeout((function(){i?t({position:e,delay:n}):o({position:e,delay:n})}),n)}))}));
-//# sourceMappingURL=03-promises.a0cc69fe.js.map
--- 03-promises.f1037c3d.js
+++
@@ -1,2 +0,0 @@
-document.querySelector('button[type = "submit"]').addEventListener("submit",(function(t){t.preventDefault(),new Promise(((t,o)=>{const i=Math.random()>.3;setTimeout((()=>{i?t({position:e,delay:n}):o({position:e,delay:n})}),n)}));var e,n}));
-//# sourceMappingURL=03-promises.f1037c3d.js.map
