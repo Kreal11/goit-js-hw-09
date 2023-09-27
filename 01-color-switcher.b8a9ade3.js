286df15c24edf014768744446875a7849bfb1e83
Kreal11 <140453416+Kreal11@users.noreply.github.com>
2023-09-27 10:44:44 +0000

    Deploying to gh-pages from @ Kreal11/goit-js-hw-09@f94e66080b1d2f308490773f2c31fa3356c5d282 🚀

--- 01-color-switcher.b8a9ade3.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");let d=null;e.disabled=!0;t.addEventListener("click",(e=>{d=setInterval((()=>{a.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(e=>{clearInterval(d),t.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.b8a9ade3.js.map
