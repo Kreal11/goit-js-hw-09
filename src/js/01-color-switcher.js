09b0c2a1e20f9d81b4cafbf4aa1e3a4b7340378c
Kreal11 <kirillshyrokov2@gmail.com>
2023-09-27 16:24:56 +0300

    update task 1 (reupdated buttons)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,9 @@
 import Notiflix from 'notiflix';
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const startBtn = document.querySelector('button[data-start]');
@@ -11,26 +13,28 @@ let timerId = null;
 
 stopBtn.disabled = true;
 
-
-const onStart = event => {
-    timerId = setInterval(() => {
-        body.style.backgroundColor = getRandomHexColor();
-    }, 1000);
-    startBtn.disabled = true;
-    stopBtn.disabled = false;
+function disableButtons(a, b) {
+  startBtn.disabled = a;
+  stopBtn.disabled = b;
 }
 
-const onStop = event => {
-    clearInterval(timerId);
-    startBtn.disabled = false;
-    Notiflix.Notify.success(
-        `Your color is ${window.getComputedStyle(body).backgroundColor}`,
-        {
-        timeout: 10000,
-        });
+function onStart() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    disableButtons(true, false);
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(timerId);
+  disableButtons(false, true);
+  Notiflix.Notify.info(
+    `Your color is ${window.getComputedStyle(body).backgroundColor}`,
+    {
+      timeout: 10000,
+    }
+  );
 }
 
 startBtn.addEventListener('click', onStart);
 stopBtn.addEventListener('click', onStop);
-
-
